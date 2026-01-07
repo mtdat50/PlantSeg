ce51e10c233723f869ca95d1213ab7bfffff1e27
mtdat50 <maitandat502@gmail.com>
2026-01-07 17:10:56 +0700

    a

---
+++ mmseg/datasets/plantsegwheat.py
@@ -0,0 +1,29 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmseg.registry import DATASETS
+from .basesegdataset import BaseSegDataset
+
+
+@DATASETS.register_module()
+class PlantSegWheatDataset(BaseSegDataset):
+    """COCO-Stuff dataset.
+
+    In segmentation map annotation for COCO-Stuff, Train-IDs of the 10k version
+    are from 1 to 171, where 0 is the ignore index, and Train-ID of COCO Stuff
+    164k is from 0 to 170, where 255 is the ignore index. So, they are all 171
+    semantic categories. ``reduce_zero_label`` is set to True and False for the
+    10k and 164k versions, respectively. The ``img_suffix`` is fixed to '.jpg',
+    and ``seg_map_suffix`` is fixed to '.png'.
+    """
+    METAINFO = dict(
+        classes=('', 'wheat bacterial leaf streak (black chaff)', 'wheat head scab',
+                 'wheat leaf rust', 'wheat loose smut', 'wheat powdery mildew', 'wheat septoria blotch',
+                 'wheat stem rust', 'wheat stripe rust'),
+
+        palette=[[0, 0, 0]] + [[i] * 3 for i in range(104, 112)])
+
+    def __init__(self,
+                 img_suffix='.jpg',
+                 seg_map_suffix='.png',
+                 **kwargs) -> None:
+        super().__init__(
+            img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
