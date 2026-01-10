ae618f54b3aeb61688f6630ee97d6e83194186ed
mtdat50 <maitandat502@gmail.com>
2026-01-11 03:21:25 +0700

    a

--- configs/segnext/segnext_mscan-l_10k_plantsegwheat.py
+++ configs/segnext/segnext_mscan-l_10k_plantsegwheat.py
@@ -27,3 +27,25 @@ model = dict(
     test_cfg=dict(mode='whole'))
 train_dataloader = dict(batch_size=4)
 train_cfg = dict(type='IterBasedTrainLoop', max_iters=40000, val_interval=4000)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00002, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        power=1.0,
+        begin=1500,
+        end=40000,
+        eta_min=0.0,
+        by_epoch=False,
+    )
+]
