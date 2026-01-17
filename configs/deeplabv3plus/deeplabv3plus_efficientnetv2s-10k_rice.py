9565246dbba2ab60f4ab4aa6ef69d769c4736360
mtdat50 <maitandat502@gmail.com>
2026-01-17 18:24:26 +0700

    a

---
+++ configs/deeplabv3plus/deeplabv3plus_efficientnetv2s-10k_rice.py
@@ -0,0 +1,83 @@
+_base_ = [
+    '../_base_/datasets/rice.py',
+    '../_base_/default_runtime.py',
+]
+train_dataloader = dict(batch_size=32)
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    bgr_to_rgb=True,
+    pad_val=0,
+    seg_pad_val=255,
+    size=(256, 256),
+    )
+model = dict(
+    type='EncoderDecoder',
+    data_preprocessor=data_preprocessor,
+    backbone=dict(
+        type='TIMMBackbone',
+        model_name='tf_efficientnetv2_s',
+        features_only=True,
+        pretrained=True,
+        in_channels=3,
+        out_indices=(1, 2, 3, 4),  # Use multiple feature levels
+    ),
+    decode_head=dict(
+        type='DepthwiseSeparableASPPHead',
+        in_channels=256,
+        in_index=3,
+        channels=512,
+        dilations=(1, 12, 24, 36),
+        c1_in_channels=48,
+        c1_channels=48,
+        dropout_ratio=0.1,
+        num_classes=9,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=160,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=9,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+# learning policy
+param_scheduler = [
+    dict(
+        type='PolyLR',
+        eta_min=1e-6,
+        power=0.9,
+        begin=0,
+        end=30000,
+        by_epoch=False)
+]
+# training schedule for 30k
+train_cfg = dict(
+    type='IterBasedTrainLoop', max_iters=30000, val_interval=1000)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=5000),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook'))
