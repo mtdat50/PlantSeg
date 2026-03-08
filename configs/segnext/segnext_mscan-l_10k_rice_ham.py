6f1dd6acffe577a2f7c94516f39bf89504c33fde
mtdat50 <maitandat502@gmail.com>
2026-03-08 21:16:48 +0700

    a

---
+++ configs/segnext/segnext_mscan-l_10k_rice_ham.py
@@ -0,0 +1,52 @@
+_base_ = './segnext_mscan-t_10k_rice.py'
+# model settings
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segnext/mscan_l_20230227-cef260d4.pth'  # noqa
+ham_norm_cfg = dict(type='GN', num_groups=32, requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    backbone=dict(
+        num_stages=5,
+        embed_dims=[64, 128, 320, 512, 512],
+        depths=[3, 5, 27, 3, 1],
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file),
+        drop_path_rate=0.3,
+        norm_cfg=dict(type='BN', requires_grad=True)),
+    decode_head=dict(
+        type='LightHamHead',
+        in_channels=[128, 320, 512, 512],
+       in_index=[1, 2, 3, 4],
+        channels=1024,
+        ham_channels=1024,
+        dropout_ratio=0.1,
+        num_classes=3,
+        norm_cfg=ham_norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
+train_dataloader = dict(batch_size=16)
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=30000, val_interval=1000)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        power=1.0,
+        begin=1500,
+        end=30000,
+        eta_min=0.0,
+        by_epoch=False,
+    )
+]
