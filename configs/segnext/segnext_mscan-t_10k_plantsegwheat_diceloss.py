ea8dc8ecdd940652dda94c909e840d832da4c147
mtdat50 <maitandat502@gmail.com>
2026-04-04 14:59:41 +0700

    a

--- configs/segnext/segnext_mscan-t_10k_plantsegwheat_diceloss.py
+++ configs/segnext/segnext_mscan-t_10k_plantsegwheat_diceloss.py
@@ -6,7 +6,7 @@ ham_norm_cfg = dict(type='GN', num_groups=32, requires_grad=True)
 model = dict(
     decode_head=dict(
         loss_decode=dict(
-            type='DiceLoss'),
+            type='DiceLoss', ignore_index=0, loss_weight=1.0),
             # type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
     ),
 )
