5a57dc0b8934e4442a13692805f2f35a9b85a181
mtdat50 <maitandat502@gmail.com>
2026-04-23 04:53:42 +0700

    a

--- configs/segnext/segnext_mscan-t_10k_plantsegwheat_weightedce.py
+++ configs/segnext/segnext_mscan-t_10k_plantsegwheat_weightedce.py
@@ -12,7 +12,9 @@ model = dict(
     decode_head=dict(
         loss_decode=[
             # dict(type='DiceLoss', ignore_index=0, loss_weight=1),
-           dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1, class_weight=[0.17, 4.19, 4.37, 3.83, 5.03, 3.16, 3.8, 4.19, 3.92], avg_non_ignore=True)
+           dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1, class_weight=[0.5, 2, 1, 1, 1, 1, 1, 1, 1], avg_non_ignore=True)
+           # dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1, class_weight=[1.09, 8.11, 8.9, 6.77, 12.36, 4.85, 6.69, 8.12, 7.1], avg_non_ignore=True)
+           # dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1, class_weight=[0.17, 4.19, 4.37, 3.83, 5.03, 3.16, 3.8, 4.19, 3.92], avg_non_ignore=True)
         ],
         ignore_index=255
     ),
