4e72a96d69c1dc990af40fcc08f59c284e2b68dc
mtdat50 <maitandat502@gmail.com>
2026-04-24 05:46:46 +0700

    a

--- configs/segnext/segnext_mscan-t_10k_wheat_encoderclshead.py
+++ configs/segnext/segnext_mscan-t_10k_wheat_encoderclshead.py
@@ -15,10 +15,10 @@ model = dict(
         in_channels=256,   # depends on backbone
         num_classes=9
     ),
-    cls_loss_weight=0.5,
+    cls_loss_weight=0.7,
     decode_head=dict(
         loss_decode=[
-           dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.5, avg_non_ignore=True)
+           dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.3, avg_non_ignore=True)
         ],
         ignore_index=255
     ),
