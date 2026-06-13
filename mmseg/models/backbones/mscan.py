7a9d8d5eb87f5d1115872ac5a94c96426fbf2716
mtdat50 <maitandat502@gmail.com>
2026-06-13 20:36:57 +0700

    a

--- mmseg/models/backbones/mscan.py
+++ mmseg/models/backbones/mscan.py
@@ -567,6 +567,25 @@ class CustomMSCAAttention10(CustomMSCAAttention8):
         return out
 
 
+class CustomMSCAAttention11(CustomMSCAAttention8):
+    def forward(self, x):
+        u = x.clone()
+
+        # Multi-Scale Feature extraction
+        attn0 = self.conv0(x)
+        attn1 = self.conv1(attn0)
+        attn2 = self.conv2(attn1)
+        attn3 = self.conv3(attn2)
+        attn4 = self.conv4(attn3)
+
+        attn = attn1 + attn2 + attn3 + attn4
+        attn = self.channel_mixing(attn)
+
+        # Convolutional Attention
+        out = attn * u
+
+        return out
+
 class MSCASpatialAttention(BaseModule):
     """Spatial Attention Module in Multi-Scale Convolutional Attention Module
     (MSCA).
@@ -635,6 +654,8 @@ class CustomMSCASpatialAttention(MSCASpatialAttention):
                 self.spatial_gating_unit = CustomMSCAAttention9(in_channels)
             case 10:
                 self.spatial_gating_unit = CustomMSCAAttention10(in_channels)
+            case 11:
+                self.spatial_gating_unit = CustomMSCAAttention11(in_channels)
 
 
 class AttentionModule(BaseModule):
