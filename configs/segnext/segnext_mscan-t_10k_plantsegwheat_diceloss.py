aa38d2cc5a260db866d436bba8db579cc171a161
mtdat50 <maitandat502@gmail.com>
2026-07-10 06:38:01 +0700

    a

--- configs/segnext/segnext_mscan-t_10k_plantsegwheat_diceloss.py
+++ configs/segnext/segnext_mscan-t_10k_plantsegwheat_diceloss.py
@@ -6,9 +6,9 @@ _base_ = [
 model = dict(
     decode_head=dict(
         loss_decode=[
-            # dict(type='DiceLoss', loss_weight=1),
-            dict(type='DiceLoss', loss_weight=0.5),
-            dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.5)
+            dict(type='DiceLoss', loss_weight=1),
+            # dict(type='DiceLoss', loss_weight=0.5),
+            # dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.5)
         ],
     ),
 )
