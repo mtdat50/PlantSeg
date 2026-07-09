8ff0bb149d3d9809c291aadaea49618142300b04
mtdat50 <maitandat502@gmail.com>
2026-07-10 06:10:22 +0700

    a

--- mmseg/engine/hooks/warmup_hook.py
+++ mmseg/engine/hooks/warmup_hook.py
@@ -7,7 +7,7 @@ class WarmupHook(Hook):
         self.warmup_iters = warmup_iters
 
     def before_train_iter(self, runner, batch_idx, data_batch=None):
-        progress = min(runner.iter / self.warmup_iters, 1.0)
+        progress = min(runner.iter / (self.warmup_iters + 1e-6), 1.0)
         for m in runner.model.modules():
             if hasattr(m, "warmup_progress"):
                 m.warmup_progress = progress
