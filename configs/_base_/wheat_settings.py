d2faff234e028618f0e1cda690fc613724f9557f
mtdat50 <maitandat502@gmail.com>
2026-07-13 20:58:41 +0700

    a

---
+++ configs/_base_/wheat_settings.py
@@ -0,0 +1,38 @@
+
+# optimizer
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        power=1.0,
+        begin=1500,
+        # begin=0,
+        end=10000,
+        eta_min=0.0,
+        by_epoch=False,
+    )
+]
+
+# training schedule for 10k
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=10000, val_interval=1000)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=10000),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook'))
---
+++ configs/mobilenet_v3/mobilenetv3-l_lraspp_wheat.py
@@ -0,0 +1,45 @@
+_base_ = [
+    '../_base_/models/lraspp_m-v3-d8.py',
+    '../_base_/datasets/plantsegwheat.py',
+    '../_base_/default_runtime.py',
+    '../_base_/wheat_settings.py'
+]
+# crop_size = (256, 256)
+crop_size = (1024, 1024)
+data_preprocessor = dict(size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    pretrained='open-mmlab://contrib/mobilenet_v3_large',
+    decode_head=dict(num_classes=9),
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole')
+)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', reduce_zero_label=False),
+    dict(
+        type='RandomResize',
+        scale=(1024, 1024),
+        ratio_range=(0.5, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='PackSegInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
+    # add loading annotation after ``Resize`` because ground truth
+    # does not need to do resize data transform
+    dict(type='LoadAnnotations', reduce_zero_label=False),
+    dict(type='PackSegInputs')
+]
+train_dataloader = dict(batch_size=1, dataset=dict(pipeline=train_pipeline))
+val_dataloader = dict(dataset=dict(pipeline=test_pipeline))
+test_dataloader = dict(dataset=dict(pipeline=test_pipeline))
+
+
+# Re-config the data sampler.
+# train_dataloader = dict(batch_size=1)
