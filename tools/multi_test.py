2efc40d44a4ae957f2d2a10d19eb2519c100d027
mtdat50 <maitandat502@gmail.com>
2026-07-18 20:11:50 +0700

    a

--- tools/multi_test.py
+++ tools/multi_test.py
@@ -1,37 +1,37 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
+import logging
 import os
 import os.path as osp
+import glob
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
-from mmengine.runner import set_random_seed
+from mmengine.utils import mkdir_or_exist, progressbar
 from mmengine.config import Config, DictAction
+from mmengine.logging import print_log
 from mmengine.runner import Runner
 
+from mmseg.registry import RUNNERS
+from mmseg.registry import DATASETS
 
-# TODO: support fuse_conv_bn, visualization, and format_only
+
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description='MMSeg test (and eval) a model')
+    parser = argparse.ArgumentParser(description='Train a segmentor')
     parser.add_argument('config', help='train config file path')
-    parser.add_argument('checkpoint', help='checkpoint file')
+    parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
-        '--work-dir',
-        help=('if specified, the evaluation metric results will be dumped'
-              'into the directory as json'))
+        '--resume',
+        action='store_true',
+        default=False,
+        help='resume from the latest checkpoint in the work_dir automatically')
     parser.add_argument(
-        '--out',
-        type=str,
-        help='The directory to save output prediction for offline evaluation')
-    parser.add_argument(
-        '--show', action='store_true', help='show prediction results')
-    parser.add_argument(
-        '--show-dir',
-        help='directory where painted images will be saved. '
-        'If specified, it will be automatically saved '
-        'to the work_dir/timestamp/show_dir')
-    parser.add_argument(
-        '--wait-time', type=float, default=2, help='the interval of show (s)')
+        '--amp',
+        action='store_true',
+        default=False,
+        help='enable automatic-mixed-precision training')
     parser.add_argument(
         '--cfg-options',
         nargs='+',
@@ -47,8 +47,6 @@ def parse_args():
         choices=['none', 'pytorch', 'slurm', 'mpi'],
         default='none',
         help='job launcher')
-    parser.add_argument(
-        '--tta', action='store_true', help='Test time augmentation')
     # When using PyTorch version >= 2.0.0, the `torch.distributed.launch`
     # will pass the `--local-rank` parameter to `tools/train.py` instead
     # of `--local_rank`.
@@ -59,26 +57,89 @@ def parse_args():
 
     return args
 
+def plot_confusion_matrix(confusion_matrix,
+                          labels,
+                          save_dir=None,
+                          show=True,
+                          title='Normalized Confusion Matrix',
+                          color_theme='OrRd'):
+    """Draw confusion matrix with matplotlib.
 
-def trigger_visualization_hook(cfg, args):
-    default_hooks = cfg.default_hooks
-    if 'visualization' in default_hooks:
-        visualization_hook = default_hooks['visualization']
-        # Turn on visualization
-        visualization_hook['draw'] = True
-        if args.show:
-            visualization_hook['show'] = True
-            visualization_hook['wait_time'] = args.wait_time
-        if args.show_dir:
-            visualizer = cfg.visualizer
-            visualizer['save_dir'] = args.show_dir
-    else:
-        raise RuntimeError(
-            'VisualizationHook must be included in default_hooks.'
-            'refer to usage '
-            '"visualization=dict(type=\'VisualizationHook\')"')
+    Args:
+        confusion_matrix (ndarray): The confusion matrix.
+        labels (list[str]): List of class names.
+        save_dir (str|optional): If set, save the confusion matrix plot to the
+            given path. Default: None.
+        show (bool): Whether to show the plot. Default: True.
+        title (str): Title of the plot. Default: `Normalized Confusion Matrix`.
+        color_theme (str): Theme of the matrix color map. Default: `winter`.
+    """
+    # normalize the confusion matrix
+    per_label_sums = confusion_matrix.sum(axis=1)[:, np.newaxis]
+    confusion_matrix = \
+        confusion_matrix.astype(np.float32) / per_label_sums * 100
 
-    return cfg
+    num_classes = len(labels)
+    fig, ax = plt.subplots(
+        figsize=(2 * num_classes, 2 * num_classes * 0.8), dpi=300)
+    cmap = plt.get_cmap(color_theme)
+    im = ax.imshow(confusion_matrix, cmap=cmap)
+    colorbar = plt.colorbar(mappable=im, ax=ax)
+    colorbar.ax.tick_params(labelsize=20)  # 设置 colorbar 标签的字体大小
+
+    title_font = {'weight': 'bold', 'size': 20}
+    ax.set_title(title, fontdict=title_font)
+    label_font = {'size': 40}
+    plt.ylabel('Ground Truth Label', fontdict=label_font)
+    plt.xlabel('Prediction Label', fontdict=label_font)
+
+    # draw locator
+    xmajor_locator = MultipleLocator(1)
+    xminor_locator = MultipleLocator(0.5)
+    ax.xaxis.set_major_locator(xmajor_locator)
+    ax.xaxis.set_minor_locator(xminor_locator)
+    ymajor_locator = MultipleLocator(1)
+    yminor_locator = MultipleLocator(0.5)
+    ax.yaxis.set_major_locator(ymajor_locator)
+    ax.yaxis.set_minor_locator(yminor_locator)
+
+    # draw grid
+    ax.grid(True, which='minor', linestyle='-')
+
+    # draw label
+    ax.set_xticks(np.arange(num_classes))
+    ax.set_yticks(np.arange(num_classes))
+    ax.set_xticklabels(labels, fontsize=20)
+    ax.set_yticklabels(labels, fontsize=20)
+
+    ax.tick_params(
+        axis='x', bottom=False, top=True, labelbottom=False, labeltop=True)
+    plt.setp(
+        ax.get_xticklabels(), rotation=45, ha='left', rotation_mode='anchor')
+
+    # draw confusion matrix value
+    for i in range(num_classes):
+        for j in range(num_classes):
+            ax.text(
+                j,
+                i,
+                '{}%'.format(
+                    round(confusion_matrix[i, j], 2
+                          ) if not np.isnan(confusion_matrix[i, j]) else -1),
+                ha='center',
+                va='center',
+                color='k',
+                size=20)
+
+    ax.set_ylim(len(confusion_matrix) - 0.5, -0.5)  # matplotlib>3.1.1
+
+    fig.tight_layout()
+    if save_dir is not None:
+        mkdir_or_exist(save_dir)
+        plt.savefig(
+            os.path.join(save_dir, 'confusion_matrix.png'), format='png')
+    if show:
+        plt.show()
 
 
 def mean_std(values):
@@ -88,9 +149,6 @@ def mean_std(values):
 
 
 def main():
-    # seeds = [2**i for i in range(20)]
-    # set_random_seed(random_seed, deterministic=False)
-
     args = parse_args()
 
     # load config
@@ -108,48 +166,65 @@ def main():
         cfg.work_dir = osp.join('./work_dirs',
                                 osp.splitext(osp.basename(args.config))[0])
 
-    cfg.load_from = args.checkpoint
+    # enable automatic-mixed-precision training
+    if args.amp is True:
+        optim_wrapper = cfg.optim_wrapper.type
+        if optim_wrapper == 'AmpOptimWrapper':
+            print_log(
+                'AMP training is already enabled in your config.',
+                logger='current',
+                level=logging.WARNING)
+        else:
+            assert optim_wrapper == 'OptimWrapper', (
+                '`--amp` is only supported when the optimizer wrapper type is '
+                f'`OptimWrapper` but got {optim_wrapper}.')
+            cfg.optim_wrapper.type = 'AmpOptimWrapper'
+            cfg.optim_wrapper.loss_scale = 'dynamic'
 
-    if args.show or args.show_dir:
-        cfg = trigger_visualization_hook(cfg, args)
+    # resume training
+    cfg.resume = args.resume
 
-    if args.tta:
-        cfg.test_dataloader.dataset.pipeline = cfg.tta_pipeline
-        cfg.tta_model.module = cfg.model
-        cfg.model = cfg.tta_model
-
-    # add output_dir in metric
-    if args.out is not None:
-        cfg.test_evaluator['output_dir'] = args.out
-        cfg.test_evaluator['keep_results'] = True
-
-
-    seeds = [2**i for i in range(20)]
 
     mious = []
     mdices = []
     mprecisions = []
     mrecalls = []
-    for seed in seeds:
-        cfg.randomness = dict(seed=seed, deterministic=False, diff_rank_seed=True)
 
-        # build the runner from config
-        runner = Runner.from_cfg(cfg)
+    # prepare confusion_matrix storage on disk
+    os.remove("confusion_matrix.npy") if os.path.exists("confusion_matrix.npy") else None
 
-        metrics = runner.test()
-        mious.append(metrics['mIoU'])
-        mdices.append(metrics['mDice'])
-        mprecisions.append(metrics['mPrecision'])
-        mrecalls.append(metrics['mRecall'])
-        # print(metrics)
+    checkpoint_paths = glob.glob(os.path.join(cfg.work_dir, 'best*'))
+
+    # start training
+    for i in range(5):
+        if 'runner_type' not in cfg:
+            # build the default runner
+            runner = Runner.from_cfg(cfg)
+        else:
+            # build customized runner from the registry
+            # if 'runner_type' is set in the cfg
+            runner = RUNNERS.build(cfg)
+
+        # load best model
+        runner.load_checkpoint(checkpoint_paths[i], map_location='cpu') 
+        runner.model.cuda()
+
+        test_seeds = [2**j for j in range(10)]
+        for test_seed in test_seeds:
+            runner.cfg.randomness = dict(seed=test_seed, deterministic=False, diff_rank_seed=True)
+
+            metrics = runner.test()
+            mious.append(metrics['mIoU'])
+            mdices.append(metrics['mDice'])
+            mprecisions.append(metrics['mPrecision'])
+            mrecalls.append(metrics['mRecall'])
 
     pd.DataFrame({
-        'seed': seeds,
         'mIoU': mious,
         'mDice': mdices,
         'mPrecision': mprecisions,
         'mRecall': mrecalls
-    }).to_csv(osp.join(cfg.work_dir, 'multi_test_results.csv'), index=False)
+    }).to_csv(osp.join(cfg.work_dir, 'multi_train_test_results.csv'), index=False)
 
     mean_miou, std_miou = mean_std(mious)
     mean_mdice, std_mdice = mean_std(mdices)
@@ -157,12 +232,23 @@ def main():
     mean_mrecall, std_mrecall = mean_std(mrecalls)
 
     print(
-        f'mIoU: {mean_miou:.4f} ± {std_miou:.4f}'
-        f'mDice: {mean_mdice:.4f} ± {std_mdice:.4f}'
-        f'mPrecision: {mean_mprecision:.4f} ± {std_mprecision:.4f}'
-        f'mRecall: {mean_mrecall:.4f} ± {std_mrecall:.4f}'
+        f'mIoU: {mean_miou:.2f} ± {std_miou:.2f}'
+        f'mDice: {mean_mdice:.2f} ± {std_mdice:.2f}'
+        f'mPrecision: {mean_mprecision:.2f} ± {std_mprecision:.2f}'
+        f'mRecall: {mean_mrecall:.2f} ± {std_mrecall:.2f}'
     )
 
+    confusion_matrix = np.load("confusion_matrix.npy")
+    dataset = DATASETS.build(cfg.test_dataloader.dataset)
+    plot_confusion_matrix(
+        confusion_matrix,
+        dataset.METAINFO['classes'],
+        save_dir=cfg.work_dir,
+        show=False,
+        title="",
+        color_theme="winter")
+
+    os.rename("confusion_matrix.npy", os.path.join(cfg.work_dir, "confusion_matrix.npy"))
 
 if __name__ == '__main__':
     main()
