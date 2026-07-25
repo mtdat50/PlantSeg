908dedde38ab0405ecca0fd1a79363f692981c23
mtdat50 <maitandat502@gmail.com>
2026-07-26 04:37:20 +0700

    a

---
+++ configs/segnext/main_custom_mscan-t_rice_nocustomspatial.py
@@ -0,0 +1,36 @@
+_base_ = [
+    'segnext_mscan-t_10k_rice.py'
+]
+
+# default spatial attn
+cls_loss_weight = 0.1
+model = dict(
+    type='EncoderDecoderWithCls',
+    backbone=dict(
+        type='MainCustomMSCAN',
+        channel_attention='ECA',
+    ),
+    cls_head=dict(
+        type='ClsHead',
+        in_channels=256,
+        num_classes=3
+    ),
+    cls_loss_weight=cls_loss_weight,
+    cls_decay_iters=10000,
+    decode_head=dict(
+        loss_decode=[
+            dict(type='DiceLoss', loss_weight=(1 - cls_loss_weight) / 2),
+            dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=(1 - cls_loss_weight) / 2)
+        ],
+        ignore_index=255
+    ),
+)
+custom_hooks = [
+    dict(
+        type='WarmupHook',
+        warmup_iters=0
+    )
+]
+
+# dataset settings
+# train_dataloader = dict(batch_size=2)
